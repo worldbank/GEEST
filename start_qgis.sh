fd715d19c62f170b0d635234bc40d7f027739215
Jeff Osundwa <jeff@kartoza.com>
2024-04-24 18:33:20 +0300

    env setup scripts

---
+++ start_qgis.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+echo "🪛 Running QGIS with the GEEST profile:"
+echo "--------------------------------"
+
+echo "  ___   ____ ___ ____             ____ _____ _____ ____ _____ "
+echo " / _ \ / ___|_ _/ ___|           / ___| ____| ____/ ___|_   _|"
+echo "| | | | |  _ | |\___ \   _____  | |  _|  _| |  _| \___ \ | |  "
+echo "| |_| | |_| || | ___) | |_____| | |_| | |___| |___ ___) || |  "
+echo " \__\_\\____|___|____/           \____|_____|_____|____/ |_|  "
+                                                              
+nix-shell -p \
+  'qgis.override { extraPythonPackages = (ps: [ ps.numpy ps.future ps.geopandas ps.rasterio ]);}' \
+  --command "qgis --profile GEEST"
