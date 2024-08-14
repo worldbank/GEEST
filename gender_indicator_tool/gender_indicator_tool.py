e0bd20618b46d4da7f8f2ff2ad22d5397907f5ed
Jeff Osundwa <jeff@kartoza.com>
2024-08-14 15:40:41 +0300

    minor update on AT

--- gender_indicator_tool/gender_indicator_tool.py
+++ gender_indicator_tool/gender_indicator_tool.py
@@ -2743,7 +2743,7 @@ class GenderIndicatorTool:
 
                 Merge = processing.run(
                     "native:mergevectorlayers",
-                    {"LAYERS": [grid_layer], "CRS": None, "OUTPUT": dif_out},
+                    {"LAYERS": [grid_layer], "CRS": None, "OUTPUT": "memory:"},
                 )
 
                 merge = Merge["OUTPUT"]
