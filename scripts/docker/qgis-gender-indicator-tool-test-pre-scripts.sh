0485405ccaf9db8ae98fce37b721555736650ecd
Jeff Osundwa <jeff@kartoza.com>
2024-09-11 17:46:17 +0300

    change parent directory name

--- scripts/docker/qgis-gender-indicator-tool-test-pre-scripts.sh
+++ scripts/docker/qgis-gender-indicator-tool-test-pre-scripts.sh
@@ -2,7 +2,7 @@
 
 qgis_setup.sh
 
-# FIX default installation because the sources must be in "qgis-gender-indicator-tool" parent folder
-rm -rf  /root/.local/share/QGIS/QGIS3/profiles/default/python/plugins/qgis-gender-indicator-tool
-ln -sf /tests_directory /root/.local/share/QGIS/QGIS3/profiles/default/python/plugins/qgis-gender-indicator-tool
-ln -sf /tests_directory /usr/share/qgis/python/plugins/qgis-gender-indicator-tool
+# FIX default installation because the sources must be in "qgis_gender_indicator_tool" parent folder
+rm -rf  /root/.local/share/QGIS/QGIS3/profiles/default/python/plugins/qgis_gender_indicator_tool
+ln -sf /tests_directory /root/.local/share/QGIS/QGIS3/profiles/default/python/plugins/qgis_gender_indicator_tool
+ln -sf /tests_directory /usr/share/qgis/python/plugins/qgis_gender_indicator_tool
