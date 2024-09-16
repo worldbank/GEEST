3dcbaf39fcb634262eb47498253cd5c6e3edbbcd
hennie <hennie@xaisim.com>
2024-09-16 15:56:46 +0200

    Introduces initial implementations of the base_dialog and widget_factory

---
+++ geest/gui/dialogs/__init__.py
@@ -0,0 +1,5 @@
+from .base_dialog import BaseDialog
+from .osm_downloader_dialog import OSMDownloaderDialog
+from .index_score_dialog import IndexScoreDialog
+
+__all__ = ['BaseDialog', 'OSMDownloaderDialog', 'IndexScoreDialog']
---
+++ geest/gui/widgets/__init__.py
@@ -0,0 +1,3 @@
+from .widget_factory import WidgetFactory
+
+__all__ = ['WidgetFactory']
