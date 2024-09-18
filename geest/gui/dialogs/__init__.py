3786f6b58f3cf181d992a6aeb0cf3fdd0db41a5e
hennie-k <hennie@kartoza.com>
2024-09-18 22:05:50 +0200

    Deletes unused directory

--- geest/gui/dialogs/__init__.py
+++
@@ -1,5 +0,0 @@
-from .base_dialog import BaseDialog
-from .osm_downloader_dialog import OSMDownloaderDialog
-from .index_score_dialog import IndexScoreDialog
-
-__all__ = ['BaseDialog', 'OSMDownloaderDialog', 'IndexScoreDialog']
