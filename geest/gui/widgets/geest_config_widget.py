1f1cc1c6e2e70ade2fef35d923f2e5cb69d5f4bf
hennie-k <hennie@kartoza.com>
2024-09-19 21:06:44 +0200

    Fixes QgsMapLayerComboBox handling in GeestConfigWidget

--- geest/gui/widgets/geest_config_widget.py
+++ geest/gui/widgets/geest_config_widget.py
@@ -4,6 +4,8 @@ from qgis.PyQt.QtWidgets import (
 )
 from qgis.PyQt.QtCore import pyqtSignal
 from qgis.gui import QgsMapLayerComboBox
+#from qgis.core import QgsVectorLayer, QgsRasterLayer
+from qgis.core import QgsProviderRegistry
 from .geest_widget_factory import GeestWidgetFactory
 
 
@@ -57,14 +59,15 @@ class GeestConfigWidget(QWidget):
         use_key = widget.property("use_key")
         if use_key:
             if isinstance(widget, QRadioButton):
-                self.widgets[use_key] = {"radio": widget}
+                if use_key not in self.widgets:
+                    self.widgets[use_key] = {}
+                self.widgets[use_key]["radio"] = widget
                 print("  " * depth + f"Stored QRadioButton for key: {use_key}")
             elif isinstance(widget, (QLineEdit, QSpinBox, QDoubleSpinBox, QComboBox, QgsMapLayerComboBox)):
-                if use_key in self.widgets:
-                    self.widgets[use_key]["widget"] = widget
-                    print("  " * depth + f"Stored {type(widget).__name__} for key: {use_key}")
-                else:
-                    print(f"Found widget for {use_key} but no associated radio button")
+                if use_key not in self.widgets:
+                    self.widgets[use_key] = {}
+                self.widgets[use_key]["widget"] = widget
+                print("  " * depth + f"Stored {type(widget).__name__} for key: {use_key}")
 
         if widget.layout():
             for i in range(widget.layout().count()):
@@ -79,15 +82,37 @@ class GeestConfigWidget(QWidget):
             widget = widgets.get("widget")
             if radio:
                 radio.toggled.connect(lambda checked, k=key: self.handle_option_change(k, checked))
+                print(f"Set up radio connection for {key}")
             if widget:
-                if isinstance(widget, QLineEdit):
+                if isinstance(widget, QgsMapLayerComboBox):
+                    print(f"Setting up connection for QgsMapLayerComboBox: {key}")
+                    widget.layerChanged.connect(lambda layer, k=key: self.update_layer_path(k, layer))
+                elif isinstance(widget, QLineEdit):
                     widget.textChanged.connect(lambda text, k=key: self.update_sub_widget_state(k, text))
                 elif isinstance(widget, (QSpinBox, QDoubleSpinBox)):
                     widget.valueChanged.connect(lambda value, k=key: self.update_sub_widget_state(k, value))
-                elif isinstance(widget, (QComboBox, QgsMapLayerComboBox)):
-                    widget.currentIndexChanged.connect(
-                        lambda index, k=key: self.update_sub_widget_state(k, widget.currentText()))
-            print(f"Set up connection for {key}")
+                elif isinstance(widget, QComboBox):
+                    widget.currentTextChanged.connect(lambda text, k=key: self.update_sub_widget_state(k, text))
+                print(f"Set up widget connection for {key}: {type(widget).__name__}")
+
+    def update_layer_path(self, key, layer):
+        print(f"update_layer_path called for {key}")  # Debug print
+        if layer:
+            provider_key = layer.providerType()
+            uri = layer.dataProvider().dataSourceUri()
+            print(f"Layer URI: {uri}")
+            decoded = QgsProviderRegistry.instance().decodeUri(provider_key, uri)
+            print(f"Decoded URI: {decoded}")
+            path = decoded.get('path') or decoded.get('url') or decoded.get('layerName')
+            if path:
+                print(f"Path found: {path}")
+                self.update_sub_widget_state(key, path)
+            else:
+                print(f"Unable to determine path for layer {layer.name()} with provider {provider_key}")
+                self.update_sub_widget_state(key, uri)  # Fallback to using the full URI
+        else:
+            print(f"No layer selected for {key}")
+            self.update_sub_widget_state(key, None)
 
     def handle_option_change(self, option, checked):
         if checked:
@@ -102,8 +127,13 @@ class GeestConfigWidget(QWidget):
         self.stateChanged.emit(self.get_state())
 
     def update_sub_widget_state(self, option, value):
-        self.modified_config[option] = value
-        self.stateChanged.emit(self.get_state())
+        if value is not None:
+            self.modified_config[option] = value
+            self.stateChanged.emit(self.get_state())
+        else:
+            print(f"Received None value for option: {option}")
+            self.modified_config[option] = "0"
+            self.stateChanged.emit(self.get_state())
 
     def get_state(self):
         return self.modified_config.copy()
