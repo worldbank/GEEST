8fcfbdf6153f3c4da5a99deb52f04560e517b25d
Tim Sutton <tim@kartoza.com>
2025-03-31 09:51:27 +0100

    Add QGIS Native Accessibility in settings Fixes #28

--- list-vscode-extensions.sh
+++ list-vscode-extensions.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
 
-code --extensions-dir=".vscode-extensions" --list-extensions | xargs -L 1 echo code --extensions-dir=".vscode-extensions" --install-extension
+# Install required extensions
+echo "code --user-data-dir='.vscode' \\"
+echo "--profile='geest' \\"
+echo "--extensions-dir='.vscode-extensions' . \\"
+code --user-data-dir='.vscode' \
+--profile='geest' \
+--extensions-dir='.vscode-extensions' . \
+--list-extensions \
+--show-versions | xargs -L 1 echo code --extensions-dir=".vscode-extensions" --install-extension 
+
+echo ""
+echo "Paste the above lines into your .vscode.sh and add a back slash at the end of each install-extension line except the last."
