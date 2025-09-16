38cebb4e42bb7d1fcf44a10f7b8065d9175c11d6
Tim Sutton <tim@kartoza.com>
2025-09-16 07:05:53 +0100

    Tidy up shellcheck run

--- scripts/compile-strings.sh
+++ scripts/compile-strings.sh
@@ -2,11 +2,9 @@
 LRELEASE=$1
 LOCALES=$2
 
-
-for LOCALE in ${LOCALES}
-do
+for LOCALE in ${LOCALES}; do
     echo "Processing: ${LOCALE}.ts"
     # Note we don't use pylupdate with qt .pro file approach as it is flakey
     # about what is made available.
-    $LRELEASE i18n/${LOCALE}.ts
+    $LRELEASE "i18n/${LOCALE}.ts"
 done
